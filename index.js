21d305d838123d33aa9d21a5d8e8ed1194a3b5c6
Murali <kodimurali3335@gmail.com>
2025-11-06 16:54:55 +0530

    made changes for Port and url with home

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const vendorRoutes = require('./routes/vendorRoutes');
 const bodyparser = require("body-parser");
 const firmRoutes = require("./routes/firmRoutes");
 const productRoutes = require("./routes/productRoutes");
+const path = require("path");
 const app =express()
-const PORT = 4000;
+const PORT = process.env.PORT||4000;
 dotenv.config();
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("connection success"))
@@ -15,6 +16,7 @@ app.use(bodyparser.json());
 app.use('/vendor',vendorRoutes);
 app.use('/firm',firmRoutes);
 app.use('/product',productRoutes);
+app.use('/uploads',express.static('uploads'));
 
 app.listen(PORT,()=>{
     console.log(`Server Started and running ar ${PORT}`);
